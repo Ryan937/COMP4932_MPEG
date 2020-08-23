48aaa0e331529c8bb836e5abb0f56aabe3b5a093
Ryan Cha <lsj937@gmail.com>
2020-08-22 18:46:14 -0700

    changes

--- 2_JPEG/Form1.cs
+++ 2_JPEG/Form1.cs
@@ -23,10 +23,10 @@ namespace _2_JPEG
             this.Cb = Cb;
             this.Cr = Cr;
         }
-}
+    }
 
 
-public partial class Form1 : Form
+    public partial class Form1 : Form
     {
         Bitmap image;
         Bitmap image2;
